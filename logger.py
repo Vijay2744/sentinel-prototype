5847104f35da26134a2001148c4f28505214af5a
Vijay2744 <kethesrinagavijay@gmail.com>
2026-04-16 20:32:53 +0530

    Add risk parameter to log function

--- logger.py
+++ logger.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
-def log(action, decision, reason):
+def log(action, decision, reason, risk):
     timestamp = datetime.now()
 
     print("------ SENTINEL LOG ------")
     print("Time:", timestamp)
     print("Action:", action)
+    print("Risk Level:", risk)
     print("Decision:", decision)
     print("Reason:", reason)
     print("--------------------------")
