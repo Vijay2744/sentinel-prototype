c63029eb4d54283cd75fb342854ce979ee218db4
Vijay2744 <kethesrinagavijay@gmail.com>
2026-04-16 20:03:20 +0530

    Replace print statements with logging

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from rules import check_rule
+from logger import log
 
 action = "READ_SENSITIVE_DATA"
 approved = False
 
 decision, reason = check_rule(action, approved)
 
-print("Action:", action)
-print("Decision:", decision)
-print("Reason:", reason)
+log(action, decision, reason)
