64a98d4a9d7b2e73083fdc322dd68956defa2c9e
Vijay2744 <kethesrinagavijay@gmail.com>
2026-04-18 20:53:53 +0530

    Remove hardcoded actions and add rule check

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-actions = [
-    {"action": "READ_SENSITIVE_DATA", "approved": False, "role": "ANALYST"},
-    {"action": "READ_SENSITIVE_DATA", "approved": True, "role": "MANAGER"},
-    {"action": "READ_PUBLIC_DATA", "approved": False, "role": "ANALYST"}
-]
+decision, reason, risk = check_rule(action, approved, item["role"])
