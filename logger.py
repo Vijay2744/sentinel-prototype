d4d8a3e3770b4b227aaa6df36b6cc810959e48d2
Vijay2744 <kethesrinagavijay@gmail.com>
2026-04-19 08:37:02 +0530

    Add role parameter to log function

--- logger.py
+++ logger.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-def log(action, decision, reason, risk):
+def log(action, decision, reason, risk, role):
     timestamp = datetime.now()
 
     print("------ SENTINEL LOG ------")
     print("Time:", timestamp)
     print("Action:", action)
+    print("Role:", role)
     print("Risk Level:", risk)
     print("Decision:", decision)
     print("Reason:", reason)
     print("--------------------------")
-print("Role:", role)
